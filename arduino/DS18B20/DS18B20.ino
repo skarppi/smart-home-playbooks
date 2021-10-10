b506cb26d8b80a39df340b50f80602c5c81dc987
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2021-10-11 00:49:14 +0300

    Add servo control for adjusting temperature of central heating.

--- arduino/DS18B20/DS18B20.ino
+++ arduino/DS18B20/DS18B20.ino
@@ -5,6 +5,7 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <TimeLib.h>
+#include <Servo.h>
 
 // Network SSID
 const char* ssid = "{{wlan_ssid}}";
@@ -16,6 +17,9 @@ const char* mqtt_server = "192.168.200.1";
 const char* temperature_topic_pannu = "sensors/pannu";
 const char* temperature_topic_patteri = "sensors/patteri";
 
+const char* command_topic = "sensors/pannu/command";
+const char* state_topic = "sensors/pannu/current";
+
 PubSubClient mqttClient(espClient);
 
 // Define NTP Client to get time
@@ -25,6 +29,40 @@ NTPClient timeClient(ntpUDP, "192.168.200.1");
 // Sensor on pin 4
 OneWire  ds(D4);
 
+// Servo
+Servo myservo;
+
+// celsius at output angle min and max
+float MIN_TEMP = 25;
+float MAX_TEMP = 45;
+
+// degress 0-180
+float MIN_ANGLE = 160;
+float MAX_ANGLE = 20;
+
+void setCurrentServo(float temp) {
+  float validTemp = constrain(temp, MIN_TEMP, MAX_TEMP);
+
+  float angle = map(validTemp, MIN_TEMP, MAX_TEMP, MIN_ANGLE, MAX_ANGLE);
+
+  Serial.print("Move servo ");
+  Serial.println(angle);
+
+  myservo.write(angle);
+}
+
+float getCurrentServo() {
+  float angle = myservo.read();
+
+  float temp = map(angle, MIN_ANGLE, MAX_ANGLE, MIN_TEMP, MAX_TEMP);
+  Serial.print("Current servo ");
+  Serial.print(angle);
+  Serial.print(" = ");
+  Serial.println(temp);
+
+  return temp;
+}
+
 void setup(void) {
   Serial.begin(9600);
 
@@ -32,6 +70,7 @@ void setup(void) {
   WiFi.hostname("ds18b20");
   WiFi.begin(ssid, password);
 
+  Serial.println("WiFi connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -44,9 +83,14 @@ void setup(void) {
   Serial.println(WiFi.localIP());
 
   mqttClient.setServer(mqtt_server, 1883);
+  mqttClient.setCallback(callback);
+  mqttClient.setKeepAlive(90);
+  mqttClient.setSocketTimeout(90);
 
   timeClient.begin();
   timeClient.update();
+
+  myservo.attach(D1);
 
   Serial.println("Setup done");
 }
@@ -54,8 +98,11 @@ void setup(void) {
 void reconnect() {
   while (!mqttClient.connected()) {
     Serial.print("Connecting to MQTT broker ...");
-    if (mqttClient.connect("DS18820")) {
+    if (mqttClient.connect("pannuhuone")) {
       Serial.println("OK");
+      if (!mqttClient.subscribe(command_topic, 1)) {
+        Serial.println("Subscribe failed");
+      }
     } else {
       Serial.print("Error : ");
       Serial.print(mqttClient.state());
@@ -80,6 +127,21 @@ char* getUTCTime() {
 }
 
 void loop(void) {
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+
+  if (!mqttClient.loop()) {
+    Serial.println("MQTT disconnected");
+  }
+
+  if (!readTemperature()) {
+    ds.reset_search();
+    delay(60000);    
+  }
+}
+
+bool readTemperature() {
   byte i;
   byte present = 0;
   byte type_s;
@@ -88,19 +150,12 @@ void loop(void) {
   float celsius;
 
   if ( !ds.search(addr)) {
-    ds.reset_search();
-    delay(60000);
-    return;
+    return false;
   }
 
-  if (!mqttClient.connected()) {
-    reconnect();
-  }
-  mqttClient.loop();
-  
   if (OneWire::crc8(addr, 7) != addr[7]) {
       Serial.println("CRC is not valid!");
-      return;
+      return false;
   }
   Serial.println();
  
@@ -120,7 +175,7 @@ void loop(void) {
       break;
     default:
       Serial.println("Device is not a DS18x20 family device.");
-      return;
+      return false;
   } 
 
   ds.reset();
@@ -168,7 +223,7 @@ void loop(void) {
 
   digitalWrite(2,HIGH); 
 
-  if (doc["id"] == "DS18B20-102") {
+  if (doc["id"] == "DS18B20-55") {
     mqttClient.publish(temperature_topic_pannu, output, true);
   } else if (doc["id"] == "DS18B20-81") {
     mqttClient.publish(temperature_topic_patteri, output, true);
@@ -180,8 +235,48 @@ void loop(void) {
     }
   }
   
-  //
-  mqttClient.disconnect();
   delay(2000);
   digitalWrite(2,LOW);
+
+  return true;
+}
+
+
+// function called when a MQTT message arrived
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+  // concat the payload into a string
+  String payload;
+  for (uint8_t i = 0; i < p_length; i++) {
+    payload.concat((char)p_payload[i]);
+  }
+  float requested = payload.toFloat();
+  
+  // handle message topic
+  Serial.print("Received message to ");
+  Serial.print(p_topic);
+  Serial.print(" value ");
+  Serial.println(payload);
+  
+  if (String(command_topic).equals(p_topic)) {
+    float current = getCurrentServo();
+    if (current != requested) {
+
+      setCurrentServo(requested);
+
+      StaticJsonDocument<200> doc;
+      doc["id"] = "MG90S";
+      doc["timestamp"] = getUTCTime();;
+      doc["current"] = requested;
+      doc["previous"] = current;
+
+      char output[256];
+      serializeJson(doc, output);
+      Serial.println(output);
+    
+      mqttClient.publish(state_topic, output, true);
+    } else {
+      Serial.print("Already at ");
+      Serial.println(current);
+    }
+  }
 }
