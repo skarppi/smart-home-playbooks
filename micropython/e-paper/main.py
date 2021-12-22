ac52ca024108fd94a9e544a96feb1c509eae2f22
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2021-12-22 23:58:14 +0200

    Put the display to deep sleep between partial refreshes.

--- micropython/e-paper/main.py
+++ micropython/e-paper/main.py
@@ -24,9 +24,6 @@ def clear():
 def full():
     clear()
 
-    epd.init(1)
-    epd.Clear(0xFF, 1)
-
     # out
     fb.text('Ulkona', 10, 0, font10)
 
@@ -45,17 +42,21 @@ def full():
 
     # downstairs
     fb.text_center('Alakerta', 220, font10)
-    # fb.rect(10, 200, w - 20, 125)
     fb.line(10, 320, w - 10, 320)
 
     # cellar
     fb.text_center('Patteri', 340, font10)
     fb.line(10, h - 10, w - 10, h - 10)
-    # fb.line(w - 10, 130, w - 10, 300)
 
-    epd.display_1Gray(fb.render(), True)
+    epd.display_1Gray(fb.render())
 
 def partial():
+    # Wake up the screen from sleep
+    epd.reset()
+
+    # Deep sleep forgets the previous buffer and partial refresh cannot know changes. Put back the old state.
+    epd.display_1Gray(fb.render(), False)
+
     fb.clear(10, 20, w // 2 - 50, 40)
     fb.text(temp('takapiha', 1), 10, 20, sfpro30)
 
@@ -71,20 +72,17 @@ def partial():
     fb.clear(30, 360, w - 60, 50)
     fb.text_center(temp('pannu', 1), 360, sfpro50)
 
-    start = time.time()
     epd.display_1Gray(fb.render())
-    print('render in {0} seconds'.format(time.time() - start))
 
-async def render_loop():
-    while True:
-        partial()
-
-        await asyncio.sleep(58)
-
+    epd.sleep()
+    
 async def main():
     asyncio.create_task(connection.start())
     full()
-    await render_loop()
+
+    while True:
+        partial()
+        await asyncio.sleep(60)
 
 try:
     asyncio.run(main())
